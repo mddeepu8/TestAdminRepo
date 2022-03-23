61d0f1b9702e639c35a8efa64971b44767c42c3f
mddeepu8 <84235132+mddeepu8@users.noreply.github.com>
2022-03-23 11:25:09 +0530

    Create admin1.py

---
+++ admin1.py
@@ -0,0 +1,17 @@
+from github import Github
+from github import get_github3_client
+# Creating this to mass update all of our repos settings.
+g = Github("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") # Your personal token
+def gh():
+    global static_gh
+    if not static_gh:
+        static_gh = get_github3_client()
+    return static_gh
+def fetch_admins(owner, repo):
+    """get all repository admins."""
+    admins = set()
+    repo = gh().repository(owner, repo)
+    for collaborator in repo.collaborators():
+        if collaborator.permissions["admin"]:
+            admins.add(collaborator.login)
+return admins
